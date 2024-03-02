b65cb6b60155082bd2f232a8a224cf61d4c5a973
Ethan Cruz <ethancruz3141@gmail.com>
2024-03-01 19:36:56 -0600

    touch ups for easier vm use

---
+++ vm_installs.sh
@@ -0,0 +1,7 @@
+sudo apt install tmux neovim python3.11-venv
+python3 -m venv ./env
+source ./env/bin/activate
+export trainDataExists=False
+export useSamplePgn=False
+export trainModel=True
+tmux new -s train
