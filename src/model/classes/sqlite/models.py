6cb402034f97cadf7bd0455e644a515d4551db50
Ethan Cruz <ethancruz3141@gmail.com>
2024-09-26 09:12:17 -0500

    working on data splitter

--- src/model/classes/sqlite/models.py
+++ src/model/classes/sqlite/models.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import declarative_base
 import json
+import hashlib
 
 Base = declarative_base()
 
@@ -19,7 +20,20 @@ class GamePositions(Base):
     white_wins = Column(Integer)
     black_wins = Column(Integer)
     stalemates = Column(Integer)
-
+    
+    @property
+    def get_hash(self):
+        game_string = (
+            self.piece_positions 
+            + self.castling_rights 
+            + self.en_passant 
+            + self.turn 
+            + str(self.greater_than_n_half_moves)
+        )
+        hash_object = hashlib.sha256(game_string.encode())
+        hex_dig = hash_object.hexdigest()
+        return hex_dig
+    
     @property
     def total_wins(self):
         total_wins = self.white_wins + self.black_wins + self.stalemates
@@ -53,31 +67,16 @@ class GamePositions(Base):
             black_wins=win_buckets["black_wins"],
             stalemates=win_buckets["stalemates"]
         )
-class GamePositionRollup(Base):
-    __tablename__ = "GamePositionRollup"
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    piece_positions = Column(String, index=True)
-    castling_rights = Column(String, index=True)
-    en_passant = Column(String, index=True)
-    turn = Column(String, index=True)
-    greater_than_n_half_moves = Column(Integer, index=True)
-    white_wins = Column(Integer)
-    black_wins = Column(Integer)
-    stalemates = Column(Integer)
 
-    @property
-    def total_wins(self):
-        total_wins = self.white_wins + self.black_wins + self.stalemates
-        return total_wins
-    
-    @property
-    def win_buckets(self):
-        total_wins = self.total_wins
-        if total_wins > 0:
-            mean_w = self.white_wins/total_wins
-            mean_b = self.black_wins/total_wins
-            mean_s = self.stalemates/total_wins
-            return [mean_w,mean_b,mean_s]
-        else: 
-            return [0,0,0]
-        
+class TrainGamePositions(GamePositions):
+    __tablename__="TrainGamePositions"
+
+class TestGamePositions(GamePositions):
+    __tablename__="TestGamePositions"
+
+class ValidationGamePositions(GamePositions):
+    __tablename__="ValidationGamePositions"
+
+class GamePositionRollup(GamePositions):
+    __tablename__ = "GamePositionRollup"
+
